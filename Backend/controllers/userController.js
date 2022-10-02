8af2dc9ce7150bb20241cd83cb3d71629ca0e7de
gyanimishra <gyan12081997@gmail.com>
2022-10-02 15:36:16 +0530

    regiter/login_apicreated

--- Backend/controllers/userController.js
+++ Backend/controllers/userController.js
@@ -1,5 +1,15 @@
 
 const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+const User = require('../models/User');
+
+const createToken = (user) => {
+	return jwt.sign({ user }, process.env.SECRET, {
+		expiresIn: '7d',
+	});
+};
 
 module.exports.registerValidation=[
     body("name").not().isEmpty().trim().withMessage("Name is reuired!"),
@@ -7,15 +17,88 @@ module.exports.registerValidation=[
     body("password").isLength({min:6}).withMessage("Password must be 6 digit!")
 ]
 
-module.exports.register= (req,res)=>{
+module.exports.register= async (req,res)=>{
     const {name,email,password}= req.body;
 
     const errors =validationResult(req);
     if(!errors.isEmpty()){
-         res.json(errors.array())
+        return res.status(400).json({ errors: errors.array() });
     }
-    else{
-        res.json("Congratulatios You have done successfully!")
-    }
+    try {
 
+        const checkUser= await User.findOne({email})
+        if(checkUser){
+            return res
+            .status(400)
+            .json({ errors: [{ msg: 'Email is already Exits' }] });
+        }
+
+        // password salt create
+        const salt= await bcrypt.genSalt(10)
+         // hash password create
+         const hash= await bcrypt.hash(password,salt)
+
+        try {
+           const user= User.create({
+            name,
+            email,
+            password:hash
+           })
+           const token = createToken(user);
+           return res.status(200).json({msg:'Your account has been created Successfuly !',token})
+
+        } catch (error) {
+            return res.status(500).json({ errors: error });
+        }
+        
+    } catch (error) {
+        return res.status(500).json({ errors: error });
+    }
+   
+
+}
+
+// login routes..........................................
+
+
+
+module.exports.loginValidation=[
+    body("email").not().isEmpty().trim().withMessage("Email is reuired!"),
+    body("password").not().isEmpty().withMessage("Password is reuired!")
+]
+
+
+
+module.exports.login= async (req,res)=>{
+   
+    const errors =validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({ errors: errors.array() });
+    }
+    const {email,password}= req.body;
+    try {
+        const user= await User.findOne({email})
+         
+        if(user)
+        {
+             const passwordMatched= await bcrypt.compare(password,user.password)
+             if(passwordMatched)
+             {
+                const token = createToken(user);
+				return res
+					.status(200)
+					.json({ msg: 'You have login successfully', token });
+             }
+             else{
+                return res.status(404).json({ errors: [{ msg: 'Password is Incorrect !!' }] });
+             }
+        }
+        else{
+            return res.status(404).json({ errors: [{ msg: 'Email is not found' }] });
+        }
+        
+        
+    } catch (error) {
+        return res.status(500).json({ errors: error });
+    }
 }
--- Backend/routes/userRoutes.js
+++ Backend/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const app = require('express');
 const router= app.Router();
-const {register,registerValidation}= require('../controllers/userController')
+const {register,registerValidation,loginValidation,login}= require('../controllers/userController')
 
 router.post('/register',registerValidation,register)
+router.post('/login',loginValidation,login)
 module.exports=router;
