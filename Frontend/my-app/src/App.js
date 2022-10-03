95d56d9abf0346b8cbce05b42e00188397f46778
gyanimishra <gyan12081997@gmail.com>
2022-10-03 12:09:50 +0530

    NavbarCompleted

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -1,7 +1,20 @@
-
+import react from "react";
+import {Routes,Route} from 'react-router-dom'
+import './main.scss';
+import Home from './components/Home';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
+        <Navbar/>
+      <Routes>
+        <Route path='/' exact element={<Home/>} /> 
+        <Route path='/register' exact element={<Register/>} /> 
+        <Route path='/login' exact element={<Login/>} /> 
+   
+      </Routes>
      
     </div>
   );
