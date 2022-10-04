be93dc232d1edad1527c3e4e0e66b9e9986647b8
gyanimishra <gyan12081997@gmail.com>
2022-10-04 14:19:59 +0530

    login/register-page-completed

--- Frontend/my-app/src/components/Auth/Login.jsx
+++ Frontend/my-app/src/components/Auth/Login.jsx
@@ -1,11 +1,59 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import BgImage from './BgImage';
+import { Helmet } from 'react-helmet';
+import { useSelector, useDispatch } from 'react-redux';
 
 const Login = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
-
-export default Login
+	
+	return (
+		<>
+			<Helmet>
+				<title>Blog-User Login</title>
+				<meta name='description' content='User login form' />
+			</Helmet>
+			<div className='row mt-80'>
+				<div className='col-8'>
+					<BgImage />
+				
+				</div>
+				<div className='col-4'>
+					<div className='account'>
+						<div className='account__section'>
+							<form >
+								<div className='group'>
+									<h3 className='form-heading'>Login</h3>
+								</div>
+								<div className='group'>
+									<input
+										type='email'
+										name='email'
+										
+										className='group__control'
+										placeholder='Enter Email'
+									/>
+								</div>
+								<div className='group'>
+									<input
+										type='password'
+										name='password'
+										
+										className='group__control'
+										placeholder='Create Password'
+									/>
+								</div>
+								<div className='group'>
+									<input
+										type='submit'
+										className='btn btn-default btn-block'
+										
+									/>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+export default Login;
--- Frontend/my-app/src/components/Auth/Register.jsx
+++ Frontend/my-app/src/components/Auth/Register.jsx
@@ -1,11 +1,68 @@
-import React from 'react'
+import { useState, useEffect } from 'react';
+import BgImage from './BgImage';
+import { Helmet } from 'react-helmet';
 
-const Register = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
 
-export default Register
+const Register = (props) => {
+	
+
+	return (
+		<>
+			<Helmet>
+				<title>User Register</title>
+				<meta name='description' content='User register form' />
+			</Helmet>
+			<div className='row mt-80'>
+				<div className='col-8'>
+					<BgImage />
+				</div>
+				<div className='col-4'>
+					<div className='account'>
+						<div className='account__section'>
+							<form >
+								<div className='group'>
+									<h3 className='form-heading'>Register</h3>
+								</div>
+								<div className='group'>
+									<input
+										type='text'
+										name='name'
+										className='group__control'
+										placeholder='Enter Name'
+										
+									/>
+								</div>
+								<div className='group'>
+									<input
+										type='email'
+										name='email'
+										className='group__control'
+										placeholder='Enter Email'
+										
+									/>
+								</div>
+								<div className='group'>
+									<input
+										type='password'
+										name='password'
+										className='group__control'
+										placeholder='Create Password'
+										
+									/>
+								</div>
+								<div className='group'>
+									<input
+										type='submit'
+										className='btn btn-default btn-block'
+									
+									/>
+								</div>
+							</form>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+};
+export default Register;
